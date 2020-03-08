74baa83a5317aaa94f03e62a02ef06cc5dee85ea
LD-solo <59973618+LD-solo@users.noreply.github.com>
2020-03-08 21:15:38 +0400

    files

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+
+$(document).ready(function(){
+  $('.landmark').height($(window).height());
+})
+
+$(document).ready(function(){
+  $('.sunset').height($(window).height());
+})
+
+$(document).ready(function(){
+  $('.vegetation').height($(window).height());
+})
